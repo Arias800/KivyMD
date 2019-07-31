e5411b193ef364e13e2947520a15d7c351771002
Arias800 <azertypoil630@gmail.com>
2019-07-31 19:54:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import re
 from distutils.core import setup
 
-VERSION_FILE = "kivymd/__init__.py"
-ver_file_data = open(VERSION_FILE, "rt").read()
+VERSION_FILE = "kivymd","__init__.py"
+ver_file_data = open('/'.join(VERSION_FILE), "rt").read()
 ver_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
 ver_reg_search = re.search(ver_regex, ver_file_data, re.M)
 
