7461711dba5dee6e67cfc6e4fe92a05e85a18626
Arias800 <azertypoil630@gmail.com>
2019-07-31 20:04:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 import re
 from distutils.core import setup
 
-VERSION_FILE = "kivymd","__init__.py"
-ver_file_data = open('/'.join(VERSION_FILE), "rt").read()
-ver_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-ver_reg_search = re.search(ver_regex, ver_file_data, re.M)
-
-if ver_reg_search:
-    version = ver_reg_search.group(1)
-else:
-    raise ValueError("Unable to find version string in {}.".format(VERSION_FILE))
-
 setup(
     name="kivymd",
     version=version,
