4ec0fc230ba359dca7343ca3d785ef61670ddca3
Arias800 <azertypoil630@gmail.com>
2019-08-02 15:22:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
 import re
-from os import path
 from distutils.core import setup
-
-VERSION_FILE = path.join('kivymd', '__init__.py')
-ver_file_data = open(VERSION_FILE, "rt").read()
-ver_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-ver_reg_search = re.search(ver_regex, ver_file_data, re.M)
-
-if ver_reg_search:
-    version = ver_reg_search.group(1)
-else:
-    raise ValueError("Unable to find version string in {}.".format(VERSION_FILE))
+from kivymd.__init__ import __version__
 
 setup(
     name="kivymd",
-    version=version,
+    version=__version__,
     description="Set of widgets for Kivy inspired by Google's Material " "Design",
     author="Andrés Rodríguez, author fork - HeaTTheatR",
     author_email="andres.rodriguez@lithersoft.com, email author fork "
